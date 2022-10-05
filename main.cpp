ef39ddd872ebb5b129485e30e6791901c5ee2cf6
jhimy <u201818218@upc.edu.pe>
2022-10-05 00:01:39 -0500

    ahora si perra

--- main.cpp
+++ main.cpp
@@ -1,2 +1,11 @@
-cout<<"holiiii";
-cou<<"holiii";
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // eres una perra
+    cout << "Hola perra" << endl;
+    cout << "Hola perra" << endl;
+
+    return 0;
+}
