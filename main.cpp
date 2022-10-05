1696c4360c4d554c9c5d14f8a02e01aaf652968f
jhimy <u201818218@upc.edu.pe>
2022-10-05 00:13:30 -0500

    jjj

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main()
     // eres una perra
     cout << "Hola perra" << endl;
     cout << "Te quiero" << endl;
-    cout<<"<3";
+    cout << "<3";
+    cout << "<3 par cuan";
 
     return 0;
 }
